2c45d2cc771d29dd26ba8fe7b01d9c506ede2511
Islom Boltaboyev <admin@.gmail.com>
2021-11-26 00:26:55 +0500

    o\zgarishlarni malaga oshirdim

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -441,7 +441,7 @@
 
                         <div class="ml-12">
                             <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                Salom
+                                Salom dunyo dsadsadadsad
                             </div>
                         </div>
                     </div>
