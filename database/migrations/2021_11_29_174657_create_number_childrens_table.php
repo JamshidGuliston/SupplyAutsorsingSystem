8d78b61ac599b2e0571db8aa581e78347742a20a
Islom Boltaboyev <admin@.gmail.com>
2021-11-29 23:17:02 +0500

    bajarildi

---
+++ database/migrations/2021_11_29_174657_create_number_childrens_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNumberChildrensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('number_childrens', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('kingar_name_id');
+            $table->integer('day_id');
+            $table->integer('king_age_name_id');
+            $table->integer('kingar_children_number');
+            $table->integer('kingar_menu_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('number_childrens');
+    }
+}
