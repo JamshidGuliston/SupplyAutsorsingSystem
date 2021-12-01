b12c142b4582cd971f81477b9722a09326d2a97a
Islom Boltaboyev <admin@.gmail.com>
2021-12-01 23:37:34 +0500

    saqlash

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -1,30 +1,35 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Day;
 use App\Models\Month;
 use App\Models\Kindgarden;
+use App\Models\Temporary;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    function index(Request $request){
-        $gr = Kindgarden::all();
+    function index(Request $request)
+    {
+        $gr = Temporary::join('kindgardens', 'temporaries.kingar_name_id', '=', 'kindgardens.id')->orderBy('kingar_name_id')->get();
 
-    	return view('adminhome', ['gardens' => $gr]);
+
+
+        return view('adminhome', ['gardens' => $gr]);
     }
 
-    public function tomorrowdate(Request $request){
+    public function tomorrowdate(Request $request)
+    {
         $days = Day::orderBy('id', 'DESC')->first();
-        if(empty($days["day_number"])){
+        if (empty($days["day_number"])) {
             $days["day_number"] = 0;
         }
-        $d=strtotime("tomorrow");
-        if(date("w", $d) != 0 and date("w", $d) != 6 and $days["day_number"] != date("d", $d)){
+        $d = strtotime("tomorrow");
+        if (date("w", $d) != 0 and date("w", $d) != 6 and $days["day_number"] != date("d", $d)) {
             echo date("d", $d);
-        }
-        else{
-            $startdate=strtotime("Monday");
+        } else {
+            $startdate = strtotime("Monday");
             date("d", $startdate);
         }
     }
--- database/migrations/2021_12_01_153312_create_temporaries_table.php
+++ database/migrations/2021_12_01_153312_create_temporaries_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePersonalAccessTokensTable extends Migration
+class CreateTemporariesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('temporaries', function (Blueprint $table) {
             $table->id();
-            $table->integer('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+            $table->integer('kingar_name_id');
+            $table->integer('age_id');
+            $table->integer('age_number');
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('temporaries');
     }
 }
