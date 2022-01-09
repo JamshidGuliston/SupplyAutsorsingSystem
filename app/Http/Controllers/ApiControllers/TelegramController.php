84e5898c9be962e6b32a812b92c89a9a4a3155d8
JamshidGuliston <jamshidbotirov92@gmail.com>
2022-01-09 17:13:46 +0300

    tgbot new

--- app/Http/Controllers/ApiControllers/TelegramController.php
+++ app/Http/Controllers/ApiControllers/TelegramController.php
@@ -9,17 +9,48 @@ use App\Traits\RequestTrait;
 
 class TelegramController extends Controller
 {
-    use MakeComponents;
-    use RequestTrait;
-    public function webhook()
-    {
-        return $this->apiRequest('setWebhook', ['url' => url(route('webhook'))]) ? ['success'] : ['something wrong'];
-    }
+    
+    public function telegrambot(){
+        $content = file_get_contents("php://input");
+        $data = json_decode($content, true);
 
-    public function index()
-    {
-        $result = json_decode(file_get_contents('php://input'));
-        $action = $result->message->text;
-        
+        if(isset($data['callback_query']))
+            $data = $data['callback_query'];
+        if(isset($data['message']))
+            $data = $data['message'];
+
+        $message = mb_strtolower(($data['text'] ? $data['text']
+            : $data['data']) , 'utf-8' );
+        $method = 'sendMessage';
+        switch ($message){
+            case '/start':
+                $send_data = [
+                    'text'=>'Hi'
+                ];
+                break;
+            default:
+                $send_data = [
+                    'text'=>'Try another text'
+                ];
+        }
+        $send_data['chat_id']=$data['chat']['id'];
+        return $this->sendTelegram($method,$send_data);
+    }
+    
+    private function sendTelegram($method,$data,$headers=[]){
+        $handle = curl_init('https://api.telegram.org/bot1249266736:AAHO9TC8CilXjkMpcbTjEk2vBNADRmUeTLM'.'/'.$method);
+        curl_setopt($handle, CURLOPT_POST, true);
+        curl_setopt($handle, CURLOPT_HEADER, 0);
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($handle, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($handle, CURLOPT_HTTPHEADER,
+            array_merge( array("Content-Type: application/json"),
+                $headers ) );
+        curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+
+        $result = curl_exec($handle);
+        curl_close($handle);
+        return ( json_decode($result,1) ? json_decode($result,1) : $result);
     }
 }
