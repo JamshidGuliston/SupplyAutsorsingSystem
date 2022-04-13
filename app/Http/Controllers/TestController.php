0e6aab820aa6fae6d4bade2fcc73f1ba3972dc7b
JamshidGuliston <jamshid_acer@mail.ru>
2022-04-13 19:55:23 +0500

    asdfgss

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -735,7 +735,7 @@ class TestController extends Controller
 				$nextdaymenuitem[$item->menu_meal_time_id][0]['mealtime'] = $item->meal_time_name; 
 				// $nextdaymenuitem[$item->menu_meal_time_id][$item->menu_food_id][$item->product_name_id] = $item->weight;
 				$nextdaymenuitem[$item->menu_meal_time_id][$item->menu_food_id][$age->id] = array($item->product_name_id => $item->weight);
-				$nextdaymenuitem[$item->menu_meal_time_id][$item->menu_food_id][$age->id][$item->product_name_id]['allcount'] = $item->weight * $menu[0]['kingar_children_number'] / $products[$item->product_name_id]['div'];
+				$nextdaymenuitem[$item->menu_meal_time_id][$item->menu_food_id][$age->id][$item->product_name_id] = array('allcount' => $item->weight * $menu[0]['kingar_children_number'] / $products[$item->product_name_id]['div']);
 				$nextdaymenuitem[$item->menu_meal_time_id][$item->menu_food_id][$age->id]['age_name'] = $menu[0]['age_name'];
 				$nextdaymenuitem[$item->menu_meal_time_id][$item->menu_food_id]['foodname'] = $item->food_name; 
 				$nextdaymenuitem[$item->menu_meal_time_id][$item->menu_food_id]['foodweight'] = $item->food_weight; 
