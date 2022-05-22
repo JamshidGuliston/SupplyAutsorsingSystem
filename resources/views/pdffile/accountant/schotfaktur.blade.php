ab2431d44799da8591a4b1c9302a1e820c2d0537
JamshidGuliston <jamshid_acer@mail.ru>
2022-05-22 14:17:44 +0500

    bad

--- resources/views/pdffile/accountant/schotfaktur.blade.php
+++ resources/views/pdffile/accountant/schotfaktur.blade.php
@@ -13,7 +13,7 @@
 	 @page { margin: 0.2in 0.8in 0in 0.3in; }
 	body{
 		font-family: DejaVu Sans;
-		font-size:12px;
+		font-size:10px;
 		/* background-image: url(images/bg.jpg); */
 		background-position: top left;
 		background-repeat: no-repeat;
