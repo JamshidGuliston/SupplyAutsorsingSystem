e9cfc0f2d461584c2068df3abaf27b5325445da5
JamshidGuliston <jamshid_acer@mail.ru>
2022-05-28 16:33:53 +0500

    uyta

--- resources/views/pdffile/accountant/svod.blade.php
+++ resources/views/pdffile/accountant/svod.blade.php
@@ -10,10 +10,10 @@
 <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> -->
 <title>Title</title>
 <style>
-	 @page { margin: 0.2in 0.8in 0in 0.3in; }
+	 @page { margin: 0.2in 0.2in 0in 0.3in; }
 	body{
 		font-family: DejaVu Sans;
-		font-size:10px;
+		font-size:9px;
 		/* background-image: url(images/bg.jpg); */
 		background-position: top left;
 		background-repeat: no-repeat;
