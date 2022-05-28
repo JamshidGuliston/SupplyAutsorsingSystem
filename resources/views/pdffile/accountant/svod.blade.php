7b858221a28ed415215903220d955f70f8bf9941
JamshidGuliston <jamshid_acer@mail.ru>
2022-05-28 17:06:12 +0500

    uytava

--- resources/views/pdffile/accountant/svod.blade.php
+++ resources/views/pdffile/accountant/svod.blade.php
@@ -13,7 +13,7 @@
 	 @page { margin: 0.2in 0.2in 0in 0.3in; }
 	body{
 		font-family: DejaVu Sans;
-		font-size:9px;
+		font-size:6px;
 		/* background-image: url(images/bg.jpg); */
 		background-position: top left;
 		background-repeat: no-repeat;
