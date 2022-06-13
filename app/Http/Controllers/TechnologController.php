c8e7a4010412e5276ec00a88c9b0ecc68aa079a0
JamshidGuliston <jamshid_acer@mail.ru>
2022-06-13 11:50:46 +0500

    yangikun xatosi1

--- app/Http/Controllers/TechnologController.php
+++ app/Http/Controllers/TechnologController.php
@@ -181,7 +181,7 @@ class TechnologController extends Controller
         $sid = Season::where('hide', 1)->first();
         // dd($sid);
         $menus = Titlemenu::all();
-        if ($day == date("d-F-Y", $d or 1)) {
+        if ($day == date("d-F-Y", $d) and 0) {
             $gr = Temporary::join('kindgardens', 'temporaries.kingar_name_id', '=', 'kindgardens.id')
                 ->orderby('kindgardens.id', 'ASC')->get();
 
