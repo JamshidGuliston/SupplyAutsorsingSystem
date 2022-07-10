a5fe850f2a86284343f1ec167c308ee550c057d2
JamshidGuliston <jamshid_acer@mail.ru>
2022-07-10 11:08:21 +0500

    iuy_1_1_

--- app/Http/Controllers/AccountantController.php
+++ app/Http/Controllers/AccountantController.php
@@ -460,7 +460,7 @@ class AccountantController extends Controller
                 }
             }
         }
-        dd($nakproducts);
+        // dd($nakproducts);
         
         $costs = bycosts::where('day_id', $costid)->where('region_name_id', Kindgarden::where('id', $id)->first()->region_id)
                 ->orderBy('day_id', 'DESC')->get();
