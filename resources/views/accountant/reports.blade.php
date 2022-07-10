3c1af97588dcbff9bcca1cfaa6d260c450b0ae63
JamshidGuliston <jamshid_acer@mail.ru>
2022-07-10 16:46:24 +0500

    iuy_1_1_1_1_1_1_1_1_1_11_1

--- resources/views/accountant/reports.blade.php
+++ resources/views/accountant/reports.blade.php
@@ -70,18 +70,15 @@
                             @endforeach
                         </select>
                     </div>
-                </div>
-                <div class="row">
                     <div class="col-sm-4">
-                        <input type="text" name="over" class="form-control" required>
+                        <input type="text" name="over" class="form-control" placeholder="Ustama %" required>
                     </div>
                     <div class="col-sm-4">
-                        <input type="text" name="nds" class="form-control" required>
+                        <input type="text" name="nds" class="form-control" placeholder="NDS %" required>
                     </div>
                     <div class="col-sm-4">
                         <button type="submit" class="btn btn-info">PDF</button>
                     </div>
-                </div>
                 </form>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-success" data-bs-dismiss="modal">Close</button>
