6bd573962a81bcf60c0db285d9160bd15ff35a29
JamshidGuliston <jamshid_acer@mail.ru>
2022-07-21 19:13:11 +0500

    yqotr_1_4_

--- resources/views/technolog/minusmultistorage.blade.php
+++ resources/views/technolog/minusmultistorage.blade.php
@@ -239,11 +239,11 @@
         </thead>
         <tbody>
             @foreach($minusproducts as $key => $row)
-            <?php $all = 0; dd($minusproducts) ?>
+            <?php $all = 0; ?>
             <tr>
                 <td>{{ $row['productname'] }}</td>
                 @foreach($days as $day)
-                    @if(isset($row[$day['id']]))
+                    @if(isset($row[$day['id']."+"]) or isset($row[$day['id']."-"]))
                         <td>
                             {{ $row[$day['id']."+"].'/'.$row[$day['id']."-"] }}
                             <i class="edites far fa-edit text-info" data-bs-toggle="modal" data-bs-target="#pcountModal" data-dayid="{{ $day->id }}" data-prodid="{{ $key }}" data-weight="{{ $row[$day['id'].'+'] }}" data-kinid="{{ $kingar->id }}" style="cursor: pointer; margin-right: 16px;"> </i>
