eb044e7a5b03444ba6761f656c830511dee6ed1f
JamshidGuliston <jamshid_acer@mail.ru>
2022-07-22 14:37:23 +0500

    inm_1_1_

--- app/Http/Controllers/TechnologController.php
+++ app/Http/Controllers/TechnologController.php
@@ -1524,12 +1524,12 @@ class TechnologController extends Controller
                     $minusproducts[$row->product_name_id][$day->id."-"] += round($row->product_weight, 3);
                 }
                 else{
-                    $minusproducts[$row->product_name_id][$day->id."+"] += round($row->product_weight, 3);
+                    $minusproducts[$row->product_name_id][$day->id."+"] = $minusproducts[$row->product_name_id][$day->id."+"] .' | '. round($row->product_weight, 3);
                 }
                 $minusproducts[$row->product_name_id]['productname'] = $row->product_name;
             }
         }
-        // dd($minusproducts);
+        dd($minusproducts);
         return view('technolog.minusmultistorage', ['minusproducts' => $minusproducts, 'kingar' => $king, 'days' => $days, 'months' => $months, 'monthid' => $ill]);   
     }
 
