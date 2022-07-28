892e73e2f7a8843f81cd38a39651438eb2d97c08
JamshidGuliston <jamshid_acer@mail.ru>
2022-07-28 06:37:31 +0500

    see_1_1_1_1_14_24_141_45

--- resources/views/storage/home.blade.php
+++ resources/views/storage/home.blade.php
@@ -13,9 +13,12 @@
             <div class="p-3 bg-white shadow-sm d-flex flex-column justify-content-around align-items-center rounded">
                 <i class="fas fa-seedling fs-1 primary-text border rounded-full secondary-bg p-2"></i>
                 <div class="text-center">
-                    <h5 class="fs-3 mb-0 mt-1">{{ $row['weight'].' '.$row['size_name']; }}</h5>
-                    <h5 class="fs-3 mb-0 mt-1">{{ $row['minusweight'].' '.$row['size_name']; }}</h5>
                     <p class="fs-4" style="font-size: 18px !important;">{{ $row['p_name']; }}</p>
+                    <h4>
+                    <i class="fas fa-long-arrow-down"></i>{{ $row['weight'].' '.$row['size_name']; }}
+                    <i class="fas fa-long-arrow-up"></i>{{ $row['minusweight'].' '.$row['size_name']; }}
+                </h4>
+                    <h5 class="fs-3 mb-0 mt-1">{{ $row['minusweight'].' '.$row['size_name']; }}</h5>
                 </div>
             </div>
         </div>
