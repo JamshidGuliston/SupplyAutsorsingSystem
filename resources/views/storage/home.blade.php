cb565b124424f2773f1a780c35c143f90a708091
JamshidGuliston <jamshid_acer@mail.ru>
2022-07-28 07:06:34 +0500

    see_1_1_1_1_14_24_141_45_17_41_

--- resources/views/storage/home.blade.php
+++ resources/views/storage/home.blade.php
@@ -12,7 +12,7 @@
         <div class="col-md-3">
             <div class="p-3 bg-white shadow-sm d-flex flex-column justify-content-around align-items-center rounded">
                 <!-- <i class="fas fa-seedling fs-1 primary-text border rounded-full secondary-bg p-2"></i> -->
-                <div class="text-center">
+                <div class="text-center" style="width: 100%">
                     <p class="fs-4" style="font-size: 22px !important;">{{ $row['p_name']; }}</p>
                 <p>
                     <i class='fas fa-arrow-down' style='color: green; width: 20%'></i><?php echo $row['weight'].' '.$row['size_name'] ?> <i class='fas fa-arrow-up' style='color: red; width: 20%'></i><?php echo round($row['minusweight'], 1).' '.$row['size_name'] ?>
