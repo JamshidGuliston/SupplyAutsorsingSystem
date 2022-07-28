e8ad1fcf78c6c544adadcbb0fedff6082e1f3a36
JamshidGuliston <jamshid_acer@mail.ru>
2022-07-28 09:45:52 +0500

    see_1_1_1_1_14_24_141_45_17_41_4_7_8_4_

--- app/Http/Controllers/StorageController.php
+++ app/Http/Controllers/StorageController.php
@@ -456,7 +456,6 @@ class StorageController extends Controller
         $products = Add_large_werehouse::where('add_group_id', $request->id)
                 ->join('products', 'products.id', '=', 'add_large_werehouses.product_id')
                 ->join('sizes', 'sizes.id', '=', 'products.size_name_id')->get();
-        dd($products);
         $document = []; 
         foreach($products as $row){
             $document[$row->product_id]['add_group_id'] = $row->add_group_id;
@@ -470,6 +469,7 @@ class StorageController extends Controller
                 return $a->sort > $b->sort;
             }
         });
+        dd($products);
         // dd($document);
         $dompdf = new Dompdf('UTF-8');
 		$html = mb_convert_encoding(view('pdffile.storage.orderskladpdf', compact('items', 'document')), 'HTML-ENTITIES', 'UTF-8');
