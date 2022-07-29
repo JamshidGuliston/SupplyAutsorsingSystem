0a92d7d2c2f554be7bb2e7af2603ce934c75c5c1
JamshidGuliston <jamshid_acer@mail.ru>
2022-07-29 21:30:14 +0500

    see_1_1_176_Op_ujex_5_5_46_87_65_9

--- app/Http/Controllers/TechnologController.php
+++ app/Http/Controllers/TechnologController.php
@@ -1757,9 +1757,8 @@ class TechnologController extends Controller
                     $find = plus_multi_storage::where('kingarden_name_d', $kind->id)
                             ->where('day_id', $day)
                             ->where('shop_id', 0)
-                            ->where('product_name_id',  99)
+                            ->where('product_name_id',  $product->id)
                             ->get();
-                    dd($find->count());
                     $t = 0;
                     foreach($find as $ff){
                         if($t != 0){
