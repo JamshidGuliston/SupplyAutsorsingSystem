ff4b131c411187ed30688043880fc5d0bcf4e885
JamshidGuliston <jamshid_acer@mail.ru>
2022-07-29 21:15:35 +0500

    see_1_1_176_Op_ujex_5_5_46_87_6

--- app/Http/Controllers/TechnologController.php
+++ app/Http/Controllers/TechnologController.php
@@ -1759,14 +1759,7 @@ class TechnologController extends Controller
                             ->where('shop_id', 0)
                             ->where('product_name_id',  $product->id)
                             ->get();
-                    $t = 0;
-                    foreach($find as $ff){
-                        if($t != 0){
-                            $ff->delete();
-                        }
-                        $t++;
-                    }
-
+                    
                     if($find->count() > 1){
                         array_push($errors, $find);
                     }
