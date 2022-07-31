916720fc40341b9da77fc0b69dabc3f938fb2d0b
JamshidGuliston <jamshid_acer@mail.ru>
2022-07-31 16:18:11 +0500

    dfrt_4_5_877_4_

--- resources/views/accountant/sidemenu.blade.php
+++ resources/views/accountant/sidemenu.blade.php
@@ -3,7 +3,7 @@
     <a href="/accountant/bigbase" class="list-group-item list-group-item-action bg-transparent second-text fw-bold {{Request::is('accountant/bigbase') ? 'active' : null }}"><i class="fas fa-database"></i> Katta sklad</a>
     <a href="/accountant/multibase" class="list-group-item list-group-item-action bg-transparent second-text fw-bold {{Request::is('accountant/multibase') ? 'active' : null }}"><i class="fas fa-database"></i> Kichkina sklad</a>
     <a href="/accountant/costs" class="list-group-item list-group-item-action bg-transparent second-text fw-bold {{Request::is('accountant/costs') ? 'active' : null }}"><i class="fas fa-coins"></i> Narx</a>
-    <a href="/accountant/reports" class="list-group-item list-group-item-action bg-transparent second-text fw-bold {{Request::is('accountant/reports') ? 'active' : null }}"><i class="fas fa-file"></i> Xisobot</a>
-    <a href="/accountant/reportsworker" class="list-group-item list-group-item-action bg-transparent second-text fw-bold {{Request::is('accountant/reportsworker') ? 'active' : null }}"><i class="fas fa-file"></i> Xisobot xodimlar</a>
+    <a href="/accountant/reports" class="list-group-item list-group-item-action bg-transparent second-text fw-bold {{Request::is('accountant/reports') ? 'active' : null }}"><i class="fas fa-cogs"></i> Xisobot</a>
+    <a href="/accountant/reportsworker" class="list-group-item list-group-item-action bg-transparent second-text fw-bold {{Request::is('accountant/reportsworker') ? 'active' : null }}"><i class="fas fa-cogs"></i> Xisobot xodimlar</a>
     <a href="/accountant/income/0" class="list-group-item list-group-item-action bg-transparent second-text fw-bold {{Request::is('accountant/income/0') ? 'active' : null }}"><i class="fas fa-coins"></i> Daromad</a>
 </div>
