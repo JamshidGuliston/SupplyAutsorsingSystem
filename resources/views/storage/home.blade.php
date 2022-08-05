db5530dd317ad7db51e6142c750de7d281fababe
JamshidGuliston <jamshid_acer@mail.ru>
2022-08-05 17:01:28 +0500

    ior_4+_9_8

--- resources/views/storage/home.blade.php
+++ resources/views/storage/home.blade.php
@@ -71,11 +71,10 @@
                 <!-- <i class="fas fa-seedling fs-1 primary-text border rounded-full secondary-bg p-2"></i> -->
                 <div class="text-center" style="width: 100%">
                     <p class="fs-4" style="font-size: 22px !important;">{{ $row['p_name']; }}</p>
-                    <p><i class='fas fa-minus' style='color: red; cursor: pointer'></i></p>
                 <p>
                     <i class='fas fa-arrow-down' style='color: green; width: 5%'></i><?php echo $row['weight'].' '.$row['size_name'] ?> <i class='fas fa-arrow-up' style='color: red; width: 5%'></i><?php echo round($row['minusweight'], 1).' '.$row['size_name'] ?>
                 </p>
-                <h4 class="fs-3 mb-0 mt-1">{{ round($row['weight'] - $row['minusweight'], 1).' '.$row['size_name']; }}</h4>
+                <h4 class="fs-3 mb-0 mt-1">{{ round($row['weight'] - $row['minusweight'], 1).' '.$row['size_name']; }} <i class='fas fa-minus' style='color: red; cursor: pointer'></i></h4>
                 </div>
             </div>
         </div>
