3e9af94e6a34747abccf30213b92187eb322dd18
JamshidGuliston <jamshid_acer@mail.ru>
2022-08-05 17:11:51 +0500

    ior_4+_9_8_1_

--- resources/views/storage/home.blade.php
+++ resources/views/storage/home.blade.php
@@ -84,12 +84,12 @@
 <div class="container-fluid px-4">
     <!-- end date -->
     <div class="row g-3 my-2">
-        @foreach($products as $row)
+        @foreach($products as $key => $row)
         <div class="col-md-3">
             <div class="p-3 bg-white shadow-sm d-flex flex-column justify-content-around align-items-center rounded">
                 <!-- <i class="fas fa-seedling fs-1 primary-text border rounded-full secondary-bg p-2"></i> -->
                 <div class="text-center" style="width: 100%">
-                    <p class="fs-4" style="font-size: 22px !important;">{{ $row['p_name']; }} <i class='fas fa-minus' style='color: red; cursor: pointer' data-delet-id='{{$item->id}}' data-bs-toggle='modal' data-bs-target='#delModal'></i></p>
+                    <p class="fs-4" style="font-size: 22px !important;">{{ $row['p_name']; }} <i class='fas fa-minus' style='color: red; cursor: pointer' data-delet-id='{{$key}}' data-bs-toggle='modal' data-bs-target='#delModal'></i></p>
                 <p>
                     <i class='fas fa-arrow-down' style='color: green; width: 5%'></i><?php echo $row['weight'].' '.$row['size_name'] ?> <i class='fas fa-arrow-up' style='color: red; width: 5%'></i><?php echo round($row['minusweight'], 1).' '.$row['size_name'] ?>
                 </p>
