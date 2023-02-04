55b97ff6cd8fc15ecd1a2295eb0db74285f32373
JamshidGuliston <jamshid_acer@mail.ru>
2023-02-04 11:41:16 +0500

    asd18

--- app/Http/Controllers/StorageController.php
+++ app/Http/Controllers/StorageController.php
@@ -398,7 +398,6 @@ class StorageController extends Controller
             }
             $product = Product::where('id', $row['product_name_id'])->first();
             if($product->category_name_id == 0 and $stop == 1){
-                // dd($product, $stop, $child_count);
                 continue;
             }
             $kindproducts[$row['product_name_id']] += $row['weight'] * $worker_count;
@@ -443,10 +442,10 @@ class StorageController extends Controller
                     $stop = 1;
                 }
                 foreach($kind->age_range as $age){
-                    $ch = Number_children::where('kingar_name_id', $garden)->where('king_age_name_id', $age->id)->orderby('day_id', 'DESC')->first();
-                    if(empty($ch)){
-                        $ch = Nextday_namber::where('kingar_name_id', $garden)->where('king_age_name_id', $age->id)->first();
-                    }
+                    // $ch = Number_children::where('kingar_name_id', $garden)->where('king_age_name_id', $age->id)->orderby('day_id', 'DESC')->first();
+                    // if(empty($ch)){
+                    $ch = Nextday_namber::where('kingar_name_id', $garden)->where('king_age_name_id', $age->id)->first();
+                    // }
                     $kindproducts[$garden] = $this->menuproduct($stop, $day[$ch['king_age_name_id']], $ch['king_age_name_id'], $ch['kingar_children_number'], $kindproducts[$garden]);
                 }
                 foreach($request->workerfoods[$tr] as $key => $val){
