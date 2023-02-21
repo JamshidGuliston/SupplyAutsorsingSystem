9d4b39154293ff54189db53c439055256470d976
JamshidGuliston <jamshid_acer@mail.ru>
2023-02-21 13:57:01 +0500

    cash5

--- resources/views/casher/sidemenu.blade.php
+++ resources/views/casher/sidemenu.blade.php
@@ -1,7 +1,7 @@
 <div class="list-group list-group-flush my-3">
-    <a href="/casher/home" class="list-group-item list-group-item-action bg-transparent second-text"><i class="fas fa-tachometer-alt me-2"></i>Bosh sahifa</a>
+    <a href="/casher/home" class="list-group-item list-group-item-action bg-transparent second-text"><i class="fas fa-coins me-2"></i>Kassa</a>
+    <a href="/casher/report" class="list-group-item list-group-item-action bg-transparent second-text fw-bold {{Request::is('casher/report') ? 'active' : null }}"><i class="fas fa-paste"></i> Hisobot</a>
     <a href="/casher/costs" class="list-group-item list-group-item-action bg-transparent second-text fw-bold {{Request::is('casher/costs') ? 'active' : null }}"><i class="fas fa-paste"></i> Katta xarajat</a>
     <a href="/casher/allcosts" class="list-group-item list-group-item-action bg-transparent second-text fw-bold {{Request::is('casher/allcosts') ? 'active' : null }}"><i class="fas fa-paste"></i> Kichik xarajat</a>
-    <a href="/casher/report" class="list-group-item list-group-item-action bg-transparent second-text fw-bold {{Request::is('casher/report') ? 'active' : null }}"><i class="fas fa-paste"></i> Hisobot</a>
     
 </div>
