5284152a7e0d185aa6708d31975263560fe31c40
JamshidGuliston <jamshidstarx@gmail.com>
2024-12-01 04:45:33 +0500

    Fix PHP syntax error in allreportpdf.blade.php

--- resources/views/pdffile/storage/allreportpdf.blade.php
+++ resources/views/pdffile/storage/allreportpdf.blade.php
@@ -112,14 +112,14 @@
 							<th scope="row">{{ $tr++ }}</th>
                             <td>{{ $row['product_name'] }}</td>
 							<td>{{ $row['size_name'] }}</td>
-							<td><?php if(isset($prevmods[$row["id"]])){
+							<td><?php if(!isset($prevmods[$row["id"]])){
 									$prevmods[$row["id"]] = 0;
 								} 
 								printf("%01.3f", $prevmods[$row["id"]]) 
 								?>
 							</td>
 							<td><?php printf("%01.3f", $row['product_weight']); ?></td>
-							<td><?php if(isset($productscount[$row["id"]])){
+							<td><?php if(!isset($productscount[$row["id"]])){
 										$productscount[$row["id"]] = 0;
 									} 
 								printf("%01.3f", $productscount[$row["id"]]); ?>
