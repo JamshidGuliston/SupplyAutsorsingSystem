908868170add6701b475f0a0c6fc42e506708f8c
JamshidGuliston <jamshidstarx@gmail.com>
2025-08-18 21:00:18 +0500

    Blade faylida mahsulot kategoriyalarini ko'rsatish uchun kod qismlari izohga olingan va foydalanuvchiga maxsus kun tanlash maydoni qo'shilgan. Shuningdek, bog'chalarni tanlash uchun yangi select elementi kiritilgan.

--- resources/views/storage/addmultisklad.blade.php
+++ resources/views/storage/addmultisklad.blade.php
@@ -67,10 +67,10 @@
                 </div>
             </div>
             <div class="modal-body foodcomposition"> 
-                @foreach($product_categories as $row)
+                <!-- @foreach($product_categories as $row)
                     <label for="maxdays[{{$row->id}}]">{{ $row->pro_cat_name }}</label>
                     <input type="number" name="maxdays[{{$row->id}}]" class="form-control" required>
-                @endforeach
+                @endforeach -->
                 <input type="number" name="maxday" placeholder="2-3 кунлик" class="form-control" required>
                 Боғчаларни танлаш
                 <select id='testSelect1' name="gardens[]" class="form-select" aria-label="Default select example" multiple required>
