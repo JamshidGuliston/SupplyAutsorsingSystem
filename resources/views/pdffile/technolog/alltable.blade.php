d270d2a2db43017ed6c1644cca988c2f9b0ef100
JamshidGuliston <jamshidstarx@gmail.com>
2025-09-07 02:16:07 +0500

    Jadvaldagi ustunlar uchun HTML kodida o'zgarishlar kiritildi: ustunlar uchun rowspan atributlari yangilandi. Dizaynni yaxshilash maqsadida o'zgarishlar amalga oshirildi.

--- resources/views/pdffile/technolog/alltable.blade.php
+++ resources/views/pdffile/technolog/alltable.blade.php
@@ -175,8 +175,8 @@
     <table class="main-table">
         <thead>
             <tr>
-                <th class="col-1" rowspan="3"></th>
-                <th class="col-2" rowspan="3">Махсулотлар номи</th>
+                <th rowspan="3"></th>
+                <th rowspan="3">Махсулотлар номи</th>
                 @php $col = 0; @endphp
                 @foreach($products as $product)
                     @if(isset($product['yes']))
