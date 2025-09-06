c7b769543fd483b8f1788fb1c477feb063d42a92
JamshidGuliston <jamshidstarx@gmail.com>
2025-09-07 01:26:52 +0500

    CSS kodini yangilash: vertikal matn usulidagi transformatsiyani 180 gradusdan -90 gradusga o'zgartirish, yangi yozish uslublarini qo'shish va dizaynni yaxshilash.

--- resources/views/pdffile/technolog/alltable.blade.php
+++ resources/views/pdffile/technolog/alltable.blade.php
@@ -149,17 +149,12 @@
 		}
 
 		.vertical-text {
-			writing-mode: vertical-rl;   /* matnni vertikal qiladi (yuqoridan pastga) */
-			-webkit-writing-mode: vertical-rl;
-			-ms-writing-mode: tb-rl;
-
-			transform: rotate(180deg);   /* teskari qilib pastdan tepaga chiqaradi */
+			display: inline-block;
+			transform: rotate(-90deg);   /* pastdan yuqoriga qarab chiqadi */
 			transform-origin: center center;
-
 			font-size: 8px;
 			text-align: center;
 			white-space: nowrap;
-			display: inline-block;
 		}
     </style>
 </head>
