80fba7e5a31d016feef30c4d0cd50981fc7b93a4
JamshidGuliston <jamshidstarx@gmail.com>
2025-09-07 00:37:03 +0500

    CSS kodini yangilash: vertikal matn usulidagi transformatsiyani 270 gradusga o'zgartirish va dizaynni yaxshilash.

--- resources/views/pdffile/technolog/alltable.blade.php
+++ resources/views/pdffile/technolog/alltable.blade.php
@@ -141,7 +141,7 @@
 			position: absolute;
 			top: 50%;
 			left: 50%;
-			/* transform: translate(-50%, -50%) rotate(90deg); */
+			transform: translate(-50%, -50%) rotate(270deg);
 			transform-origin: center;
 			white-space: nowrap;
 			font-size: 6px;
