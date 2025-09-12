c03b9828417a47e3bfff8a74300f0e486dcc9a69
JamshidGuliston <jamshidstarx@gmail.com>
2025-09-12 09:07:03 +0500

    reportregion.blade.php faylida hisobot sarlavhasi yangilandi, bu esa dizaynni soddalashtirish va aniqroq ma'lumot taqdim etishga yordam beradi.

--- resources/views/pdffile/accountant/reportregion.blade.php
+++ resources/views/pdffile/accountant/reportregion.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chinoz tumani maktabgacha ta'lim tashkilotlariga Avgust oyida ko'rsatilgan Autsorsing xizmati xisob kitobi</title>
+    <title>tuman maktabgacha ta'lim tashkilotlariga</title>
     <style>
         body {
             font-family: Arial, sans-serif;
