7d8ae2b3e8b9240dde858c63f1d8a572bbcd3d68
JamshidGuliston <jamshidstarx@gmail.com>
2025-09-12 18:58:57 +0500

    nakapit.blade.php faylida matn o'lchami 7px dan 6px ga o'zgartirildi, bu esa hisobotning ko'rinishini yanada yaxshilashga yordam beradi.

--- resources/views/pdffile/accountant/nakapit.blade.php
+++ resources/views/pdffile/accountant/nakapit.blade.php
@@ -13,7 +13,7 @@
 	 @page { margin: 0.2in 0.2in 0.2in 0.2in; }
 	body{
 		font-family: DejaVu Sans;
-		font-size:7px;
+		font-size:6px;
 		/* background-image: url(images/bg.jpg); */
 		background-position: top left;
 		background-repeat: no-repeat;
