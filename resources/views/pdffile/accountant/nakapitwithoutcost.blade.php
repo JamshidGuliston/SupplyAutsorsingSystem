4c78675f1d46a5ae79e32015bd0f866f04d6f168
JamshidGuliston <jamshidstarx@gmail.com>
2025-09-13 14:33:23 +0500

    nakapitwithoutcost.blade.php faylida ustunlar orasidagi bo'shliqni yaxshilash uchun CSS o'zgartirildi. Bu o'zgarish dizaynni yanada chiroyli va ma'lumotlarni ko'rsatishni aniqroq qiladi.

--- resources/views/pdffile/accountant/nakapitwithoutcost.blade.php
+++ resources/views/pdffile/accountant/nakapitwithoutcost.blade.php
@@ -60,15 +60,13 @@
   	
   	/* Create two equal columns that floats next to each other */
   .column {
+	margin-top: 5px;
     float: left;
     text-align: center;
     width: 33%;
   }
 
   /* Clear floats after the columns */
-  .row{
-	margin-top: 15px;
-  }
   .row:after {
     content: "";
     display: table;
