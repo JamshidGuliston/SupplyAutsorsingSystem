097fb342863c869403a28869b3dab9cfc9051137
JamshidGuliston <jamshidstarx@gmail.com>
2025-09-14 14:29:39 +0500

    AccountantController.php faylida Age_range ma'lumotlarini olish jarayoni yangilandi, endi ular id bo'yicha kamayish tartibida to'g'ridan-to'g'ri olinadi. Bu o'zgarishlar kodni soddalashtiradi va samaradorligini oshiradi.

--- app/Http/Controllers/AccountantController.php
+++ app/Http/Controllers/AccountantController.php
@@ -2064,7 +2064,7 @@ class AccountantController extends Controller
             ->join('months', 'months.id', '=', 'days.month_id')
             ->join('years', 'years.id', '=', 'days.year_id')
             ->get(['days.id', 'days.day_number', 'months.month_name', 'years.year_name', 'days.created_at']);
-        $ages = Age_range::all()->orderBy('id', 'desc');
+        $ages = Age_range::orderBy('id', 'desc')->get();
         $costs = Protsent::where('region_id', $id)
                 ->where('start_date', '<=', $days[0]->created_at->format('Y-m-d'))
                 ->where('end_date', '>=', $days[count($days)-1]->created_at->format('Y-m-d'))
