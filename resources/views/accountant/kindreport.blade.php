96d4df30d39db8a474631addccbbafc2bdbe3787
JamshidGuliston <jamshidstarx@gmail.com>
2025-10-06 23:27:26 +0500

    fix: update report links in kindreport view and add new routes for dalolatnoma functionality

--- resources/views/accountant/kindreport.blade.php
+++ resources/views/accountant/kindreport.blade.php
@@ -458,10 +458,10 @@
             html += '<div class="report-category">';
             html += '<h6><i class="fas fa-file-invoice"></i>Dalolatnoma</h6>';
             html += '<div class="report-links">';
-            html += '<a href="/accountant/schotfaktursecond/'+kindid+'/'+start+'/'+end+'" target="_blank" class="report-link pdf">';
+            html += '<a href="/accountant/dalolatnoma/'+kindid+'/'+start+'/'+end+'" target="_blank" class="report-link pdf">';
             html += '<i class="far fa-file-pdf"></i>PDF';
             html += '</a>';
-            html += '<a href="/accountant/allschotfakturexcel/'+kindid+'/'+start+'/'+end+'/'+cost+'" target="_blank" class="report-link excel">';
+            html += '<a href="/accountant/dalolatnomaexcel/'+kindid+'/'+start+'/'+end+'/'+cost+'" target="_blank" class="report-link excel">';
             html += '<i class="far fa-file-excel"></i>Excel';
             html += '</a>';
             html += '</div>';
--- routes/web.php
+++ routes/web.php
@@ -343,6 +343,8 @@ Route::group(['prefix' => 'accountant', 'middleware' => ['isAccountant', 'auth']
     Route::get('schotfaktur/{id}/{ageid}/{start}/{end}/{costid}', [AccountantController::class, 'schotfaktur'])->name('accountant.schotfaktur');
     Route::get('schotfaktursecond/{id}/{start}/{end}', [AccountantController::class, 'schotfaktursecond'])->name('accountant.schotfaktursecond');
     Route::get('schotfaktursecondexcel/{id}/{start}/{end}', [AccountantController::class, 'schotfaktursecondexcel'])->name('accountant.schotfaktursecondexcel');
+    Route::get('dalolatnoma/{id}/{start}/{end}', [AccountantController::class, 'dalolatnoma'])->name('accountant.dalolatnoma');
+    Route::get('dalolatnomaexcel/{id}/{start}/{end}', [AccountantController::class, 'dalolatnomaexcel'])->name('accountant.dalolatnomaexcel');
     Route::get('allschotfaktur/{id}/{start}/{end}/{costid}/{nds}/{ust}', [AccountantController::class, 'allschotfaktur'])->name('accountant.allschotfaktur');
     Route::get('schotfakturexcel/{id}/{ageid}/{start}/{end}/{costid}/{nds}/{ust}', [AccountantController::class, 'schotfakturexcel'])->name('accountant.schotfakturexcel');
     Route::get('norm/{id}/{ageid}/{start}/{end}/{costid}', [AccountantController::class, 'norm'])->name('accountant.norm');
