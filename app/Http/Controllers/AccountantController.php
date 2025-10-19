3860217acce0c11638b3a1f632b9616e7bf56071
JamshidGuliston <jamshidstarx@gmail.com>
2025-10-19 07:15:55 +0500

    fix: remove unnecessary PDF options in AccountantController to streamline PDF generation process

--- app/Http/Controllers/AccountantController.php
+++ app/Http/Controllers/AccountantController.php
@@ -2784,11 +2784,6 @@ class AccountantController extends Controller
                     $pdf_menu->setOption('page-size', 'A4');
                     $pdf_menu->setOption('orientation', 'landscape');
                     $pdf_menu->setOption('encoding', 'UTF-8');
-                    $pdf_menu->setOption('enable-local-file-access', true);
-                    $pdf_menu->setOption('print-media-type', true);
-                    $pdf_menu->setOption('disable-smart-shrinking', false);
-                    $pdf_menu->setOption('dpi', 96);
-                    $pdf_menu->setOption('zoom', 0.8);
                     
                     $file_menu = $tempDir . '/0_menu_' . $day->id . '_' . $age->id . '_' . $menu_counter . '_' . $timestamp . '.pdf';
                     file_put_contents($file_menu, $pdf_menu->output());
