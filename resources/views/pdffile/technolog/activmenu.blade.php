404db8d0e559c3f18a7396ed32a0b9fb4bb17614
JamshidGuliston <jamshidbotirov1992@gmail.com>
2025-11-04 12:02:03 +0500

    sds dfdf fgbbfg fgbbfg

--- resources/views/pdffile/technolog/activmenu.blade.php
+++ resources/views/pdffile/technolog/activmenu.blade.php
@@ -447,9 +447,6 @@
 								@endif
 							</div>
 						@endif
-						    <div class="column">
-								
-							</div>
 							<div class="column">
 								<p style="text-align: left;"><strong>{{ explode(',', env('MENU_SIGNATURE'))[0] }}:</strong> __________________;</p>
 							</div>
