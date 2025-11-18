aeb8bb6ef5120a649fe5a2c4cecc4f8a67accac5
JamshidGuliston <jamshidstarx@gmail.com>
2025-11-19 01:22:44 +0500

    refactor: enhance PDF layout and styling in nakapitwithoutcost and spendedkg views for better readability and structure

--- resources/views/pdffile/accountant/spendedkg.blade.php
+++ resources/views/pdffile/accountant/spendedkg.blade.php
@@ -1,68 +1,50 @@
 <!DOCTYPE html>
-<html lang="uz">
+<html lang="en">
 <head>
 <meta charset="UTF-8">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Nakapit without cost</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="Description" content="Enter your description here"/>
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css"> -->
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> -->
+<title>Title</title>
 <style>
-	@page { 
-		size: A4 portrait;
-		margin: 5mm 5mm 3mm 5mm;
-	}
-	
-	* {
-		margin: 0;
-		padding: 0;
-		box-sizing: border-box;
-	}
-	
-	body {
-		font-family: 'DejaVu Sans', Arial, sans-serif;
-		font-size: 9px;
+	 @page { margin: 0.2in 0.2in 0.2in 0.2in; }
+	body{
+		font-family: DejaVu Sans;
+		font-size:6px;
+		/* background-image: url(images/bg.jpg); */
+		background-position: top left;
+		background-repeat: no-repeat;
+		background-size: 100%;
+		/* padding: 300px 100px 10px 100px; */
 		width: 100%;
-		padding: 5px;
 	}
-	
-	.header-text {
-		text-align: center;
-		font-weight: bold;
-		font-size: 10px;
-		margin-bottom: 10px;
-		line-height: 1.4;
-	}
-	
-	table {
+	table{
 		border-collapse: collapse;
-		border: 2px solid #000;
-		width: 100%;
-		table-layout: fixed;
-		font-size: 8px;
+		border: 2px solid black;
+		width: 100%;	
 	}
-	
-	thead {
-		border: 2px solid #000;
+	thead{
+		border: 2px solid black;
 	}
-	
-	th {
-		border: 1px solid #000;
-		padding: 3px 2px;
+	td {
 		text-align: center;
-		vertical-align: middle;
-		font-weight: bold;
-		white-space: nowrap;
-	}
-	
-	th.date-header {
-		height: 60px;
 		width: auto;
-		min-width: 30px;
-		text-align: center;
-		vertical-align: middle;
-		position: relative;
-		padding: 5px;
+		overflow: hidden;
+		word-wrap: break-word;
 	}
-	
-	th.date-header span {
+	th{
+		border: 1px solid black;
+		padding: 0px;
+	}
+	td{
+		border-right: 1px solid black;
+		border-bottom: 1px solid black;
+		padding: 0px;
+	}
+	.vrt-header span{
 		display: inline-block;
 		text-align: center;
 		-webkit-transform: rotate(-90deg);
@@ -71,205 +53,131 @@
 		-o-transform: rotate(-90deg);
 		transform: rotate(-90deg);
 		white-space: nowrap;
-		line-height: 1.2;
-		width: 50px;
-		height: 15px;
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		margin-top: -7.5px;
-		margin-left: -25px;
-		transform-origin: center center;
 	}
-	
-	td {
-		border: 1px solid #000;
-		padding: 2px 3px;
-		text-align: center;
-		vertical-align: middle;
-		white-space: nowrap;
-	}
-	
-	td.product-name {
-		text-align: left;
-		padding-left: 5px;
-		white-space: normal;
-		word-wrap: break-word;
-		max-width: 150px;
-	}
-	
-	/* Ustun kengliklari */
-	th:nth-child(1), td:nth-child(1) { width: 3%; }  /* № */
-	th:nth-child(2), td:nth-child(2) { width: 28%; } /* Mahsulot nomi */
-	th:nth-child(3), td:nth-child(3) { width: 5%; } /* O'lchov birligi */
-	th:last-child, td:last-child { width: 8%; }      /* Jami */
-	
-	/* Sanalar ustunlari uchun */
-	th.date-header, td.date-cell {
-		width: 7%;
-		min-width: 30px;
-		white-space: wrap;
-	}
-	
-	.size-name {
-		max-width: 50px;
-		white-space: wrap;
-		word-wrap: break-word;
-	}
-	
-	tr:first-child th {
-		font-size: 8px;
-		font-weight: bold;
-	}
-	
-	.footer-section {
-		margin-top: 20px;
-		width: 100%;
-		display: table;
-	}
-	
-	.footer-column {
-		display: table-cell;
-		width: 33.33%;
-		text-align: center;
-		padding: 5px;
-		font-size: 9px;
-	}
-	
 	.page-break {
 		page-break-after: always;
 	}
-	
-	/* Print optimizations */
-	@media print {
-		body {
-			-webkit-print-color-adjust: exact;
-			print-color-adjust: exact;
-		}
-		
-		table {
-			page-break-inside: auto;
-		}
-		
-		tr {
-			page-break-inside: avoid;
-			page-break-after: auto;
-		}
-	}
+  	
+  	/* Create two equal columns that floats next to each other */
+  .column {
+    float: left;
+    text-align: center;
+    width: 50%;
+  }
+
+  /* Clear floats after the columns */
+  .row:after {
+    content: "";
+    display: table;
+    clear: both;
+  }
 </style>
 </head>
 <body>
-	<?php
-		$numberofchildren = 0;
-		if($days[0]->month_id % 12 == 0){
-			$mth = 12;
-		}else{
-			$mth = $days[0]->month_id % 12;
-		}
-		$month = App\Models\Month::where('id', $days[0]->month_id)->first();
-	?>
-	
-	<div class="header-text">
-		{{ $kindgar->kingar_name }} да <?php printf('%04d', $days->first()->year_name) ?> йил <?php echo $days->first()->day_number."-".$days->last()->day_number ?> <?php echo $month->month_name ?> кунларида сарфланган озиқ-овқат маҳсулотлар тўғрисида маълумот
-	</div>
-	
-	<table>
-		<thead>
-			<tr>
-				<th rowspan="2">№</th>
-				<th colspan="2">Махсулотлар номи ва Ўлчов бирлиги</th>
-				<th colspan="{{ count($days) }}">Болалар сони</th>
-				<th rowspan="2">Жами</th>
-			</tr>
-			<tr>
-				@foreach($days as $day)
-					<?php
-						if($day->month_id % 12 == 0){
-							$month_id = 12;
-						}else{
-							$month_id = $day->month_id % 12;
-						}
+    <div class="container-fluid">
+        <div class="row mt-5">
+            <div class="col-md-12">
+                <div class="table" id="table_with_data">
+					<div class="col-md-3">
+						<a href="#">
+							<i class="fas fa-store-alt" style="color: dodgerblue; font-size: 18px;"></i>
+						</a>
+						<?php
+
+						    $numberofchildren = 0;
+
+							if($days[0]->month_id % 12 == 0){
+								$mth = 12;
+							}else{
+								$mth = $days[0]->month_id % 12;
+							}
+						?>
+						<center><b>{{ env('COMPANY_NAME') }} томонида Аутсорсинг хизмати кўрсатилаётган {{ $region->region_name }} {{ $kindgar->number_of_org }}-сонли ДМТТнинг <?php echo $days->first()->day_number  ?>.<?php printf('%02d', $mth) ?>.<?php printf('%02d', $costs[0]->year_name) ?> йилдан <?php echo $days->last()->day_number ?>.<?php printf('%02d', $mth) ?>.<?php printf('%02d', $costs[0]->year_name) ?> йилгача  {{ $age->age_name }}ли гуруҳи учун НАКАПИТЕЛ</b></center>
+					</div>
+                </div>
+                <table style="width:100%; table-layout: fixed;">
+                    <thead>
+                        <tr>
+                            <th scope="col" style="width: 10%;" rowspan="2">Махсулотлар</th>
+                            <th style="width: 10px;">Сана</th>
+                            <!-- <th style="width: 30px; font-size: 7px"><bold>Нарх</bold></th> -->
+                            @foreach($days as $day)
+								<th scope="col">{{ $day->day_number; }}</th>
+							@endforeach
+							<th>Жами</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+					<?php 
+						$kgsumm = 0;
+						$costsumm = 0;
+						$ndssumm = 0;
 					?>
-					<th class="date-header">
-						<span>{{ sprintf("%02d.%02d.%d", $day->day_number, $month_id, $day->year_name) }}</span>
-					</th>
-				@endforeach
-			</tr>
-		</thead>
-		<tbody>
-			<?php 
-				$kgsumm = 0;
-				$costsumm = 0;
-				$ndssumm = 0;
-				$row_number = 0;
-			?>
-			@foreach($nakproducts as $key => $row)
-				<tr>
-					@if($row['product_name'] != "Болалар сони")
-						<td>{{ ++$row_number }}</td>
-						<td class="product-name">{{ $row['product_name'] }}</td>
-						<td class="size-name">{{ $row['size_name'] ?? '' }}</td>
-					@else
-						<td colspan="3"><strong>{{ $row['product_name'] }}</strong></td>
-					@endif
-					
-					<?php $summ = 0; ?>
-					
-					@foreach($days as $day)
-						@if(isset($row[$day['id']]))
-							<td class="date-cell">
-							@if($row['product_name'] == "Болалар сони")
-								<strong>{{ $row[$day['id']] }}</strong>
+					@foreach($nakproducts as $key => $row)
+					<tr>
+						<td style="text-align: left; padding-left: 2px">{{ implode(' ', array_slice(explode(' ', $row['product_name']), 0, 3)) }}</td>
+						@if($row['product_name'] != "Болалар сони")
+							<td>{{ $row['size_name'] }}</td>
+						@endif
+						<?php 
+							$summ = 0;
+						?>
+						@foreach($days as $day)
+							@if(isset($row[$day['id']]))
+								<td>
+								@if($row['product_name'] == "Болалар сони")
+									<strong>{{ $row[$day['id']]; }}</strong>
 								<?php  
 									$summ += $row[$day['id']];
 									$numberofchildren += $row[$day['id']];
 								?>
+								@else
+                                	@if($row['size_name'] == "дона")
+                                  		<?php  
+                                            printf("%01.3f", $row[$day['id']]); 
+                                            $summ += $row[$day['id']];
+                                        ?>
+                                	@else
+                                		<?php  
+                                            printf("%01.3f", $row[$day['id']]); 
+                                            $summ += $row[$day['id']];
+                                        ?>
+                                  	@endif
+								@endif
+								</td>
 							@else
-								<?php  
-									printf("%01.3f", $row[$day['id']]); 
-									$summ += $row[$day['id']];
-								?>
+								<td>
+									{{ '0' }}
+								</td>
 							@endif
-							</td>
+						@endforeach
+                        @if($row['product_name'] != "Болалар сони")
+                      		<?php $kgsumm += $summ; ?>
+                        @endif
+						@if($row['product_name'] == "Болалар сони")
+							<td>{{ $summ }}</td>
 						@else
-							<td class="date-cell">0</td>
+							<td ><?php printf("%01.3f", $summ) ?></td>
 						@endif
+						
+					</tr>
 					@endforeach
-					
-					@if($row['product_name'] != "Болалар сони")
-						<?php $kgsumm += $summ; ?>
-					@endif
-					
-					@if($row['product_name'] == "Болалар сони")
-						<td><strong>{{ $summ }}</strong></td>
-					@else
-						<td><?php printf("%01.3f", $summ) ?></td>
-					@endif
-				</tr>
-			@endforeach
-		</tbody>
-	</table>
-	
-	<div class="footer-section">
-		<div class="footer-column">
-			<strong>Аутсорсер:</strong><br>
-			{{ env('COMPANY_NAME') ?? 'B.Tajibaev' }}<br>
-			директори: {{ env('COMPANY_DIRECTOR') ?? 'B.Tajibaev' }} ________________________
-		    <br/>
-			<br/>
-			<br/>
-			<strong>Бухгалтер:</strong> ____________________
-		</div>
-		<div class="footer-column">
-			<strong>{{ $kindgar->number_of_org ?? '3' }}-сон ДМТТ рахбари:</strong> ____________________
-			<br/>
-			<br/>
-			<br/>
-			<strong>Ошпаз:</strong> ____________________
-		</div>
-		<div class="footer-column">
-			<strong>Хамшира:</strong> ____________________
-		</div>
-	</div>
-</body>
-</html>
+				</tbody>
+			</table>
+					<div class="column">
+					    @php
+							$qrImage = base64_encode(file_get_contents(public_path('images/qrmanzil.jpg')));
+						@endphp
+						<img src="data:image/jpeg;base64,{{ $qrImage }}" 
+							style="width:120; position:absolute; left:10px;">
+					</div>
+					<div class="column">
+						<p></p>
+						<p style="text-align: center;"><strong>ДМТТ рахбари: </strong> __________________;</p>
+					</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</bod>
+<html>
