2ecdde81af46a832393d1a2e8a354822b5e970b0
JamshidGuliston <jamshidbotirov1992@gmail.com>
2026-04-28 14:32:05 +0500

    feat(roles): add role constants enum (chef=6, addelkadir=8)

---
+++ app/Constants/Roles.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Constants;
+
+class Roles
+{
+    public const BOSS = 2;
+    public const TECHNOLOG = 3;
+    public const STORAGE = 4;
+    public const ACCOUNTANT = 5;
+    public const CHEF = 6;
+    public const CASHER = 7;
+    public const ADDELKADIR = 8;
+}
